2f25846b83c4984b4dd8f934185c49fc51805294
Juan Cruz Vincenti <juan.vincenti@avature.net>
2020-07-31 13:30:24 -0300

    Modify dev environment * Moved hosts creation to dev yml * Modify create urls script * Add base host domain to dev.env

--- nginx/server_hosts/create-urls.sh
+++ nginx/server_hosts/create-urls.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-export BASE_HOST_DOMAIN=$1
-
 envsubst '$${BASE_HOST_DOMAIN}' < /etc/nginx/conf.d/server_hosts/admin-url.conf.template > /etc/nginx/conf.d/server_hosts/admin-url.conf
 envsubst '$${BASE_HOST_DOMAIN}' < /etc/nginx/conf.d/server_hosts/gateway-url.conf.template > /etc/nginx/conf.d/server_hosts/gateway-url.conf
 envsubst '$${BASE_HOST_DOMAIN}' < /etc/nginx/conf.d/server_hosts/konga-url.conf.template > /etc/nginx/conf.d/server_hosts/konga-url.conf
-nginx -g 'daemon off;'
