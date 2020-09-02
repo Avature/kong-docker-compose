d35c2012aea4583c91ef0fa06699efa69bd372ac
Juan Cruz Vincenti <juan.vincenti@avature.net>
2020-09-02 11:43:00 -0300

    Remove conf.d from path

--- nginx/server_hosts/create-urls.sh
+++ nginx/server_hosts/create-urls.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-envsubst '$${BASE_HOST_DOMAIN}' < /etc/nginx/conf.d/server_hosts/admin-url.conf.template > /etc/nginx/conf.d/server_hosts/admin-url.conf
-envsubst '$${BASE_HOST_DOMAIN}' < /etc/nginx/conf.d/server_hosts/gateway-url.conf.template > /etc/nginx/conf.d/server_hosts/gateway-url.conf
-envsubst '$${BASE_HOST_DOMAIN}' < /etc/nginx/conf.d/server_hosts/konga-url.conf.template > /etc/nginx/conf.d/server_hosts/konga-url.conf
+envsubst '$${BASE_HOST_DOMAIN}' < /etc/nginx/server_hosts/admin-url.conf.template > /etc/nginx/server_hosts/admin-url.conf
+envsubst '$${BASE_HOST_DOMAIN}' < /etc/nginx/server_hosts/gateway-url.conf.template > /etc/nginx/server_hosts/gateway-url.conf
+envsubst '$${BASE_HOST_DOMAIN}' < /etc/nginx/server_hosts/konga-url.conf.template > /etc/nginx/server_hosts/konga-url.conf
