93b91f642f8f86f4e3e73e579031febcce5bffed
Mariano Fresno <mariano.fresno@avature.net>
2020-10-23 19:24:29 -0300

    Fix admin conf to reply a JSON and allow instance registering without authentication

--- example_curl_call_including_client_cert.sh
+++ example_curl_call_including_client_cert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -X GET --insecure https://admin.kong-server.com/services/adminApi \
-  --cert nginx/certs_for_client_auth/mydomain.com.crt\
+curl -X GET --insecure https://admin-kong-server.com/services/adminApi \
+  --cert nginx/certs_for_client_auth/mydomain.com2.crt\
   --key nginx/certs_for_client_auth/each-client.key\
   --header 'apikey: replace-this-string-with-your-api-key'\
   --header 'Content-Type: application/json'\
