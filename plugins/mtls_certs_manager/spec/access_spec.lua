55adcaa886a79f893be2ef8a407a86a8edc627e3
Mariano Fresno <mariano.fresno@avature.net>
2020-10-23 10:48:19 -0300

    Add cannot load ca certificate test case, add multi execution mockery to fwk helper

--- plugins/mtls_certs_manager/spec/access_spec.lua
+++ plugins/mtls_certs_manager/spec/access_spec.lua
@@ -13,12 +13,9 @@ local load_default_dependencies = function ()
 end
 
 describe("mtls_certs_manager access hook feature", function()
-  teardown(function()
-    _G.package.loaded['kong.plugins.mtls_certs_manager.x509_name_helper'] = nil
-    _G.package.loaded["resty.openssl.x509"    ] = nil
-    _G.package.loaded["resty.openssl.x509.csr"] = nil
-    _G.package.loaded["resty.openssl.pkey"    ] = nil
-    _G.package.loaded["resty.openssl.bn"      ] = nil
+  before_each(function()
+    load_default_dependencies()
+    helper.clear_runs()
   end)
 
   it("should reject already registered instance", function()
@@ -26,7 +23,6 @@ describe("mtls_certs_manager access hook feature", function()
     helper.mock_return('kong.request', 'get_body', '{ instance = { name = "test_instance", description = "test_description"}, csr = "test_csr_string" }')
     helper.mock_return('kong.response', 'exit', '{}')
 
-    load_default_dependencies()
     local subject = require('access')
 
     local conf = {
@@ -41,12 +37,11 @@ describe("mtls_certs_manager access hook feature", function()
     assert.spy(_G.kong.response.exit).was_called_with(401, {message = "Instance already exists"})
   end)
 
-  it("should reject unknown instance with empty or nil csr request", function()
+  it("should reject unknown instance with empty csr request", function()
     helper.mock_return('kong.db.consumers', 'select_by_username', 'nil')
     helper.mock_return('kong.request', 'get_body', '{ instance = { name = "test_instance", description = "test_description"}, csr = "" }')
     helper.mock_return('kong.response', 'exit', '{}')
 
-    load_default_dependencies()
     local subject = require('access')
 
     local conf = {
@@ -59,14 +54,28 @@ describe("mtls_certs_manager access hook feature", function()
 
     subject.execute(conf)
     assert.spy(_G.kong.response.exit).was_called_with(400, {message = "CSR Contents are empty"})
+  end)
+
+  it("should reject unknown instance with nil csr request", function()
+    helper.mock_return('kong.db.consumers', 'select_by_username', 'nil')
     helper.mock_return('kong.request', 'get_body', '{ instance = { name = "test_instance", description = "test_description"}, csr = nil }')
+    helper.mock_return('kong.response', 'exit', '{}')
+
+    local subject = require('access')
+
+    local conf = {
+      ca_private_key_path = "./example_certs/CA-key.pem",
+      ca_certificate_path = "",
+      ca_private_key_passphrase = "test_passphrase"
+    }
+
+    spy.on(_G.kong.response, "exit")
 
     subject.execute(conf)
     assert.spy(_G.kong.response.exit).was_called_with(400, {message = "CSR Contents are empty"})
   end)
 
   it("should reject invalid non-empty CSR contents", function()
-    load_default_dependencies()
     helper.mock_return('kong.db.consumers', 'select_by_username', 'nil')
     helper.mock_return('kong.request', 'get_body', '{ instance = { name = "test_instance", description = "test_description"}, csr = "something-invalid-for-csr" }')
     helper.mock_return('kong.response', 'exit', '{}')
@@ -89,7 +98,6 @@ describe("mtls_certs_manager access hook feature", function()
   end)
 
   it("should reject CSR with no subject", function()
-    load_default_dependencies()
     helper.mock_return('kong.db.consumers', 'select_by_username', 'nil')
     helper.mock_return('kong.request', 'get_body', '{ instance = { name = "test_instance", description = "test_description"}, csr = "valid-csr-but-contains-no-subject" }')
     helper.mock_return('kong.response', 'exit', '{}')
@@ -113,7 +121,6 @@ describe("mtls_certs_manager access hook feature", function()
   end)
 
   it("should reject CSR with no public key attached", function()
-    load_default_dependencies()
     helper.mock_return('kong.db.consumers', 'select_by_username', 'nil')
     helper.mock_return('kong.request', 'get_body', '{ instance = { name = "test_instance", description = "test_description"}, csr = "valid-csr-but-contains-no-pubkey" }')
     helper.mock_return('kong.response', 'exit', '{}')
@@ -138,7 +145,6 @@ describe("mtls_certs_manager access hook feature", function()
   end)
 
   it("should reject CSR with public key not matching signature", function()
-    load_default_dependencies()
     helper.mock_return('kong.db.consumers', 'select_by_username', 'nil')
     helper.mock_return('kong.request', 'get_body', '{ instance = { name = "test_instance", description = "test_description"}, csr = "valid-csr-pubkey-not-matches-signature" }')
     helper.mock_return('kong.response', 'exit', '{}')
@@ -164,7 +170,6 @@ describe("mtls_certs_manager access hook feature", function()
   end)
 
   it("should reject instance_name not matching csr subject's common name", function()
-    load_default_dependencies()
     helper.mock_return('kong.db.consumers', 'select_by_username', 'nil')
     helper.mock_return('kong.request', 'get_body', '{ instance = { name = "this_test_instance_name", description = "test_description"}, csr = "valid-csr-pubkey-not-matches-signature" }')
     helper.mock_return('kong.response', 'exit', '{}')
@@ -194,7 +199,6 @@ describe("mtls_certs_manager access hook feature", function()
   end)
 
   it("should reject registration because failed in serial number creation", function()
-    load_default_dependencies()
     helper.mock_return('kong.db.consumers', 'select_by_username', 'nil')
     helper.mock_return('kong.request', 'get_body', '{ instance = { name = "mydomain.com", description = "test_description"}, csr = "valid-csr-pubkey-not-matches-signature" }')
     helper.mock_return('kong.response', 'exit', '{}')
@@ -235,5 +239,52 @@ describe("mtls_certs_manager access hook feature", function()
 
     subject.execute(conf)
     assert.spy(_G.kong.response.exit).was_called_with(400, {message = "Error creating serial number", error_description = "Problem creating BN!"})
+  end)
+
+  it("should reject being unable to load the CA certificate", function()
+    helper.mock_return('kong.db.consumers', 'select_by_username', 'nil')
+    helper.mock_return('kong.request', 'get_body', '{ instance = { name = "mydomain.com", description = "test_description"}, csr = "valid-csr-pubkey-not-matches-signature" }')
+    helper.mock_return('kong.response', 'exit', '{}')
+    helper.mock_return('parsed_csr_mock', 'get_subject_name', '{}, nil')
+    helper.mock_return('parsed_csr_mock', 'get_pubkey', '{}, nil')
+    local mocked_bn = helper.mock_return('resty.openssl.bn', 'new', '{}, nil')
+    helper.mock_return('mocked_file_resource', 'read', '\'contents\'')
+    helper.mock_return('mocked_file_resource', 'close')
+    helper.mock_return('io', 'open', 'mocked_file_resource')
+    helper.mock_return('parsed_csr_mock', 'verify', 'true, nil')
+    local name_helper_mock = helper.mock_return('kong.plugins.mtls_certs_manager.x509_name_helper', 'tostring', '\'C=US, ST=CA, CN=mydomain.com/O="MyOrg, Inc."\'')
+    local csr_mock = helper.mock_return('resty.openssl.x509.csr', 'new', 'parsed_csr_mock, nil')
+    helper.mock_return('mocked_crt_object', 'set_subject_name')
+    helper.mock_return('mocked_crt_object', 'set_pubkey')
+    helper.mock_return('mocked_crt_object', 'set_not_after')
+    helper.mock_return('mocked_crt_object', 'set_not_before')
+    helper.mock_return('mocked_crt_object', 'set_serial_number')
+    helper.mock_return('ngx', 'time', '1111111')
+    local mocked_pkey = helper.mock_return('resty.openssl.pkey', 'new', '{}')
+
+    helper.mock_return('resty.openssl.x509', 'new', 'mocked_crt_object')
+    local mocked_x509 = helper.mock_return('resty.openssl.x509', 'new', 'nil, "error parsing ca"', 1)
+
+    _G.package.loaded["resty.openssl.x509.csr"] = csr_mock
+    _G.package.loaded["resty.openssl.x509"] = mocked_x509
+    _G.package.loaded['kong.plugins.mtls_certs_manager.x509_name_helper'] = name_helper_mock
+    _G.package.loaded["resty.openssl.bn"] = mocked_bn
+    _G.package.loaded["resty.openssl.pkey"] = mocked_pkey
+    _G.package.loaded["access"] = nil
+
+    local subject = require('access')
+
+    local conf = {
+      ca_private_key_path = "./example_certs/CA-key.pem",
+      ca_certificate_path = "",
+      ca_private_key_passphrase = "test_passphrase",
+      common_name_regex = "CN=(.*)/O=",
+      days_of_validity = 60
+    }
+
+    spy.on(_G.kong.response, "exit")
+
+    subject.execute(conf)
+    assert.spy(_G.kong.response.exit).was_called_with(500, {message = "Cannot load CA certificate", error_description = "error parsing ca"})
   end)
 end)
