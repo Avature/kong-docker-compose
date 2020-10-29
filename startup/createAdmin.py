b4b41e31fe475596700e674997b6a696f37e2a30
Mariano Fresno <mariano.fresno@avature.net>
2020-10-29 19:18:31 -0300

    Modify route and services creation to detect 404 * Add kong conf to debian

--- startup/createAdmin.py
+++ startup/createAdmin.py
@@ -13,18 +13,26 @@ admin_api_plugins = [
 ]
 
 def create_admin_service():
-  admin_api_payload = {"name": "adminApi", "protocol": "http", "port": 8001, "host": "127.0.0.1"}
-  requests.post(url=admin_base_url + '/services', data=admin_api_payload, verify=False)
+  admin_api_response = requests.get(admin_base_url + '/services/adminApi')
+  if admin_api_response.status_code == 404:
+    admin_api_payload = {"name": "adminApi", "protocol": "http", "port": 8001, "host": "127.0.0.1"}
+    response = requests.post(url=admin_base_url + '/services', data=admin_api_payload, verify=False)
+    if response.status_code != 201:
+      exit(1)
 
 def create_admin_route():
-  create_admin_service()
-  payload = {"name": "adminApi", "protocols": ["http", "https"], "paths": ["/admin-api"]}
-  response = requests.post(admin_base_url + '/services/adminApi/routes', data=payload, verify=False)
+  create_route('/admin-api', 'adminApi')
 
 def create_register_instance_route():
-  create_admin_service()
-  payload = {"name": "adminApiRegisterInstance", "protocols": ["http", "https"], "paths": ["/admin-api/instances/register"], "methods": ["POST"]}
-  response = requests.post(admin_base_url + '/services/adminApi/routes', data=payload, verify=False)
+  create_route('/admin-api/instances/register', 'adminApiRegisterInstance')
+
+def create_route(route_path, route_name):
+  route_response = requests.get(admin_base_url + '/services/adminApi/routes' + route_name)
+  if route_response.status_code == 404:
+    payload = {"name": route_name, "protocols": ["http", "https"], "paths": [route_path], "methods": ["POST"]}
+    response = requests.post(admin_base_url + '/services/adminApi/routes', data=payload, verify=False)
+    if response.status_code != 201:
+      exit(1)
 
 def get_api_key():
   config = configparser.ConfigParser()
@@ -39,17 +47,15 @@ def create_consumer():
   if admin_consumer_response.status_code == 200 and admin_consumer_response.json()["data"]:
     if admin_consumer_response.json()["data"][0]["key"] != api_key:
       requests.delete(key_auth_url + '/' + admin_consumer_response.json()["data"][0]["id"])
-      requests.post(key_auth_url, data={"key": api_key}, verify=False)
   else:
     requests.post(admin_base_url + '/consumers', data={"username": "admin"}, verify=False)
     requests.post(key_auth_url, data={"key": api_key}, verify=False)
 
 def create_admin():
   create_consumer()
-  admin_route_response = requests.get(admin_base_url + '/services/adminApi')
-  if admin_route_response.status_code == 404:
-    create_admin_route()
-    create_register_instance_route()
+  create_admin_service()
+  create_admin_route()
+  create_register_instance_route()
   add_plugins()
 
 def get_previous_plugins_for_target(target):
