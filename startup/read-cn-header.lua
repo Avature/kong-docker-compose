39ed65f59507973c006d07dccf7f2ae36c086bc3
Mariano Fresno <mariano.fresno@avature.net>
2020-11-11 08:38:06 -0300

    :fix: Add switch to createDevCerts for creating dev ssl * Fix readme services * Add to nginx ssl verify depth * Remove unused lambda pre-function * Restore secrets config * Move client consumer plugin to adminApi route avoiding register instance to get broken

--- startup/read-cn-header.lua
+++
@@ -1,4 +0,0 @@
--- This script reads the header that NGINX defined with the CN of the Client Cert
--- and puts it on a new header of the response, showing that all the circuit works OK
-local value_with_the_cn = kong.request.get_header('X-Certificate-CN-Header')
-kong.response.set_header('X-output-response-header', value_with_the_cn)
