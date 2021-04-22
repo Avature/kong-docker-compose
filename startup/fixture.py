1134f2311b769365f81ce309f381e33d89f53b3a
Juan Cruz Vincenti <juan.vincenti@avature.net>
2021-04-22 15:53:31 -0300

    Remove usage of apikey and consumer

--- startup/fixture.py
+++ startup/fixture.py
@@ -1,7 +1,4 @@
-import json
 import requests
-import os
-import configparser
 import json
 import logging
 from config import Config
@@ -36,27 +33,6 @@ class Fixture:
     if route_response.status_code == 404:
       payload = {"name": route_name, "protocols": ["http", "https"], "paths": [route_path], "methods": methods}
       self.post_or_fail(admin_base_url + '/services/adminApi/routes', data=payload, verify=False)
-
-  def get_api_key(self):
-    config = configparser.ConfigParser()
-    api_key_file = os.environ["kong_apikey_file"]
-    config.read(api_key_file)
-    return config['kong-apikey']['apikey']
-
-  def create_consumer(self):
-    api_key = self.get_api_key()
-    key_auth_url = admin_base_url + '/consumers/admin/key-auth'
-    admin_consumer_response = requests.get(key_auth_url)
-    if admin_consumer_response.status_code == 200 and admin_consumer_response.json()["data"]:
-      if admin_consumer_response.json()["data"][0]["key"] != api_key:
-        self.__renew_api_key(api_key, key_auth_url, admin_consumer_response.json()["data"][0]["id"])
-    else:
-      self.post_or_fail(admin_base_url + '/consumers', data={"username": "admin"}, verify=False)
-      self.post_or_fail(key_auth_url, data={"key": api_key}, verify=False)
-
-  def __renew_api_key(self, api_key, key_auth_url, id):
-    requests.delete(key_auth_url + '/' + id)
-    self.post_or_fail(key_auth_url, data={"key": api_key}, verify=False)
 
   def get_previous_plugins_for_target(self, target):
     if target not in self.previously_installed_plugins:
@@ -96,7 +72,6 @@ class Fixture:
     return response
 
   def run(self):
-    self.create_consumer()
     self.create_admin_service()
     self.create_admin_route()
     self.create_register_instance_route()
