09ecfe67fa8d36b50696de4481f2525bdb2a6206
mariano.fresno <mariano.fresno@avature.net>
2021-09-12 03:16:28 -0300

    Add db initialization script to configMap * Fix apply script

--- kubernetes/apply.sh
+++ kubernetes/apply.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-kubectl apply -f kong-data-volume.yaml -f konga-deploy.yaml -f kong-deploy.yaml -f db-deploy.yaml -f ingress.yaml -f secrets.yaml
-
+kubectl apply -f secrets.yaml -f kong-data-volume.yaml -f db-deploy.yaml -f kong-deploy.yaml -f konga-deploy.yaml -f ingress.yaml
