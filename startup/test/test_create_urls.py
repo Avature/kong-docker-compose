be3a9214430bcbec1a67fbca6bb69074e274a32b
mariano.fresno <mariano.fresno@avature.net>
2021-12-22 12:35:39 -0300

    Skip init on tests of startup

--- startup/test/test_create_urls.py
+++ startup/test/test_create_urls.py
@@ -12,22 +12,22 @@ if (os.environ.get("ENABLE_DEBUGGER")):
 class TestCreateUrls(TestCase):
   def setUp(self):
     self.test_path = "/nginx_startup/server_hosts/test/"
-    os.environ["TARGET_URLS_CONF_DIR"] = self.test_path + "/"
+    os.environ["TARGET_URLS_CONF_DIR"] = self.test_path
     Path(self.test_path).mkdir(parents=True, exist_ok=True)
 
   def tearDown(self):
     shutil.rmtree(self.test_path)
 
   def test_files_not_exist(self):
-    self.assertFileNotExists(self.test_path + "/admin-url.conf")
-    self.assertFileNotExists(self.test_path + "/gateway-url.conf")
-    self.assertFileNotExists(self.test_path + "/konga-url.conf")
+    self.assertFileNotExists(self.test_path + "admin-url.conf")
+    self.assertFileNotExists(self.test_path + "gateway-url.conf")
+    self.assertFileNotExists(self.test_path + "konga-url.conf")
 
   def test_run_create_urls(self):
-    subprocess.call("/app/init.sh")
-    adminUrlContent = self.read_file(self.test_path + "/admin-url.conf")
-    gatewayUrlContent = self.read_file(self.test_path + "/gateway-url.conf")
-    kongaUrlContent = self.read_file(self.test_path + "/konga-url.conf")
+    subprocess.call("/nginx_startup/server_hosts/create-urls.sh")
+    adminUrlContent = self.read_file(self.test_path + "admin-url.conf")
+    gatewayUrlContent = self.read_file(self.test_path + "gateway-url.conf")
+    kongaUrlContent = self.read_file(self.test_path + "konga-url.conf")
     self.assertEqual('server_name admin-sepa-test-host-for-testing;\n', adminUrlContent)
     self.assertEqual('server_name gateway-sepa-test-host-for-testing kong-gateway;\n', gatewayUrlContent)
     self.assertEqual('server_name konga-sepa-test-host-for-testing;\n', kongaUrlContent)
