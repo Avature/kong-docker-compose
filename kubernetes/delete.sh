f668201e7b0998b032fe2b0e8dc78a36e0695f19
gonzalo.decos <gonzalo.decos@avature.net>
2022-02-09 16:23:50 -0300

    Add scripts to run K8s local

--- kubernetes/delete.sh
+++ kubernetes/delete.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo -n "Press ENTER key if you sure that you want to remove all the environment from your cluster. Otherwise press Ctrl+C to Cancel."
-read
-kubectl delete -f kong-data-volume.yaml -f konga-deploy.yaml -f kong-deploy.yaml -f db-deploy.yaml -f secrets.yaml -f ingress.yaml.template
-
+read -p "Are you sure that you want to remove all the environment from your cluster? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  kubectl delete -f kong-data-volume.yaml -f konga-deploy.yaml -f kong-deploy.yaml -f db-deploy.yaml -f secrets.yaml -f ingress.yaml.template
+else
+  echo -e "Operation cancelled"
+fi
