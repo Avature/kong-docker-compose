0d336a95c32fe889f5bcefcbcf42d99877e7b321
mariano.fresno <mariano.fresno@avature.net>
2022-04-13 19:41:49 -0300

    Fix create-urls to work from app directory * Fix permission setting * Support sh instead of bash

--- nginx/create-urls.sh
+++ nginx/create-urls.sh
@@ -1,8 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 cd `dirname "$0"`
 
+echo "Creating URL config files from templates..."
+
 TARGET_URLS_CONF_DIR="${TARGET_URLS_CONF_DIR:-./}"
+
+cd $TARGET_URLS_CONF_DIR
 
 envsubst '$${HOST_SERVICE_SEPARATOR} $${BASE_HOST_DOMAIN}' < ./admin-url.conf.template > "$TARGET_URLS_CONF_DIR"admin-url.conf
 envsubst '$${HOST_SERVICE_SEPARATOR} $${BASE_HOST_DOMAIN}' < ./gateway-url.conf.template > "$TARGET_URLS_CONF_DIR"gateway-url.conf
 envsubst '$${HOST_SERVICE_SEPARATOR} $${BASE_HOST_DOMAIN}' < ./konga-url.conf.template > "$TARGET_URLS_CONF_DIR"konga-url.conf
+
+echo "Finished configuring server URLs"
--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-/etc/nginx/conf.d/server_hosts/create-urls.sh
-nginx -g daemon off; "@$"
+echo "Starting Avature NGINX"
+
+/app/create-urls.sh
+
+nginx -g 'daemon off;'
