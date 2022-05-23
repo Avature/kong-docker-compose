2b2e76e2a7f9e1ea793e6869bfb6d360a11e3a3c
Mariano Fresno <mariano.fresno@avature.net>
2022-05-23 14:08:18 -0300

    Add docker-compose installation to the script * Fix debian ignored paths

--- testAdminContract.sh
+++ testAdminContract.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+echo "Installing Docker-Compose"
+
+curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+
+chmod +x /usr/local/bin/docker-compose
+
+echo "Running Contract Tests..."
+
 docker-compose --env-file dev.env -f docker-compose.yml -f docker-compose.test.yml up
