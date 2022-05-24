1c84ddd97cc7a3f134235055a8879f0ef6a209b5
Mariano Fresno <mariano.fresno@avature.net>
2022-05-24 01:18:16 -0300

    Convert some values into env variables for config

--- admin_contract/test/test_verify_contracts.py
+++ admin_contract/test/test_verify_contracts.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from pact import Verifier
+import os
 
 PACT_BROKER_URL = "http://localhost:9292"
 PACT_BROKER_USERNAME = "pactbroker"
@@ -12,9 +13,9 @@ PROVIDER_URL = f"https://{PROVIDER_HOST}:{PROVIDER_PORT}"
 class TestVerifyContracts(TestCase):
   def __broker_opts(self):
     return {
-      "broker_username": PACT_BROKER_USERNAME,
-      "broker_password": PACT_BROKER_PASSWORD,
-      "broker_url": PACT_BROKER_URL,
+      "broker_username": os.environ.get("PACT_BROKER_USERNAME") or PACT_BROKER_USERNAME,
+      "broker_password": os.environ.get("PACT_BROKER_PASSWORD") or PACT_BROKER_PASSWORD,
+      "broker_url": os.environ.get("PACT_BROKER_URL") or PACT_BROKER_URL,
       "publish_version": "0.0.19",
       "publish_verification_results": True,
     }
