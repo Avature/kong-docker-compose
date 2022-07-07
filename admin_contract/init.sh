02fdc277822449732529db4df5d85690384618e7
mariano.fresno <mariano.fresno@avature.net>
2022-07-07 12:46:11 -0300

    Fine-tune timeouts to avoid sporadics

--- admin_contract/init.sh
+++ admin_contract/init.sh
@@ -15,7 +15,7 @@ export -f healthcheck
 
 echo "Waiting Kong to be ready for tests..."
 
-timeout 180s bash -c healthcheck
+timeout 300s bash -c healthcheck
 
 if [[ $? -eq 124 ]]; then
   echo "Waiting for Kong has timed out"
