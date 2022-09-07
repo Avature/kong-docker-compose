edbd08c64cc13961cff0f33692ddd14e0f7a8e2a
josemanuel.hidalgo <josemanuel.hidalgo@avature.net>
2022-09-07 17:26:30 +0200

    Used wait-for-postgres.sh script for wait for postgree in all points needed.

--- check/make-kong-migrations.sh
+++ check/make-kong-migrations.sh
@@ -6,18 +6,7 @@ then
   rm /check/finished
 fi
 
-echo ---- ---- KONG_PG_HOST $KONG_PG_HOST
-echo ---- ---- KONG_PG_PORT $KONG_PG_PORT
-echo ---- ---- KONG_PG_USER $KONG_PG_USER
-echo ---- ---- KONG_PG_PASSWORD $KONG_PG_PASSWORD
-echo "---- ---- ---- ---- Waiting for PostgreSQL - starting ---- ---- ---- ----"
-until nc -zvw10 $KONG_PG_HOST $KONG_PG_PORT 2>&1 >/dev/null
-do
-  echo "---- ---- ---- ---- Waiting for PostgreSQL - sleep 1 ---- ---- ---- ----"
-  sleep 1
-done
-
-echo "---- ---- Postgresql is ready. Starting bootstrapping and migrations ... ---- ----"
+/check/wait-for-postgres.sh
 
 kong migrations bootstrap 
 kong migrations up 
--- check/wait-for-postgres.sh
+++ check/wait-for-postgres.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # wait-for-postgres.sh
 
+echo ---- ---- KONG_PG_HOST $KONG_PG_HOST
+echo ---- ---- KONG_PG_PORT $KONG_PG_PORT
 echo "---- ---- ---- ---- Waiting for PostgreSQL - starting ---- ---- ---- ----"
 until nc -zvw10 $KONG_PG_HOST $KONG_PG_PORT 2>&1 >/dev/null
 do
