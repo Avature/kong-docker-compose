4eb5e2e56a60617bd9c32575cf306513b711b50c
josemanuel.hidalgo <josemanuel.hidalgo@avature.net>
2022-09-20 22:37:48 +0200

    Prove to run kong migrations without sincro, as in Github repos.

--- kong/conf/custom-entrypoint.sh
+++ kong/conf/custom-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-/check/wait-for-migrations.sh
+#/check/wait-for-migrations.sh
+/check/wait-for-postgres.sh
 
 chown kong:nogroup /home/kong/certs
 /docker-entrypoint.sh kong docker-start
--- startup/init.sh
+++ startup/init.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-echo "---- ---- ---- ---- Waiting for PostgreSQL... ---- ---- ---- ----"
-until nc -zvw10 $KONG_PG_HOST $KONG_PG_PORT 2>&1 >/dev/null
-do
-  echo "---- ---- ---- ---- Waiting for PostgreSQL... ---- ---- ---- ----"
-  sleep 1
-done
-echo "---- ---- ---- ---- Postgresql is ready ... ---- ---- ---- ----"
+/check/wait-for-postgres.sh
 
 bash -c "/certs_startup/createCerts.sh"
 
