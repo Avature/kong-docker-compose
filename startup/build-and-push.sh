be51ccfeca8ed29f9dcd40ee1d4af14aca3d2824
Mariano Fresno <mariano.fresno@avature.net>
2023-01-16 18:32:35 -0300

    Add config env var to support external plugins configs

--- startup/build-and-push.sh
+++ startup/build-and-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $(dirname $0)
 cp ../check/wait-for-postgres.sh ./
-docker build . -t ghcr.io/avature/kong-startup:0.0.5
-docker push ghcr.io/avature/kong-startup:0.0.5
+docker build --network=host . -t ghcr.io/avature/kong-startup:0.0.7
+docker push ghcr.io/avature/kong-startup:0.0.7
 rm ./wait-for-postgres.sh
