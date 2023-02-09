8cf58f96ad15592d1d4abb920f49a89fedbb1017
Mariano Fresno <mariano.fresno@avature.net>
2023-02-08 22:39:47 -0300

    Make UID and GID dynamic by env vars and configurable via puppet

--- kong/conf/custom-entrypoint.sh
+++ kong/conf/custom-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 /wait-for-postgres.sh $KONG_PG_HOST $KONG_PG_PORT
+
+usermod -u ${KNG_UID:-1000} kong_usr
+groupmod -g ${KNG_GID:-1000} kong_grp
+
 touch /var/log/admin-api.log
 chmod 666 /var/log/admin-api.log
 chown kong_usr:kong_grp /home/kong/certs
--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 echo "-> Starting Avature NGINX"
 
+usermod -u ${NGX_UID:-1000} ngx_usr
+groupmod -g ${NGX_GID:-1000} ngx_grp
+
 /etc/ssl/createCerts.sh
 
 /app/create-urls.sh
