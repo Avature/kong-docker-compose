d35701a4f747bfd42ed58d6e75b277b39c914b6f
Mariano Fresno <mariano.fresno@avature.net>
2023-02-08 23:05:22 -0300

    Fix dynamic mod of group and users

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-echo "-> Starting Avature NGINX"
+
+echo "-> Setting up UID GID for NGINX"
 
 usermod -u ${AVATURE_KONG_UID:-1000} ngx_usr
 groupmod -g ${AVATURE_KONG_GID:-1000} ngx_grp
+
+echo "-> Starting Avature NGINX"
 
 /etc/ssl/createCerts.sh
 
